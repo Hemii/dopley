30d6f5ec713428fa8337349a1a96eafdabfffa18
H3MII <rusnak.anton3@gmail.com>
2020-05-21 15:16:01 +0200

    first deploy

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# dopley
+# deploy
---
+++ script.js
@@ -0,0 +1,51 @@
+new Vue({ // vo vue komponent sa musis odkazovat cez slovicko this.
+          //v html davaj bez this
+  el: "#app",
+  data: {
+    title: "WPUBproject RSS Reader",
+    desc: "Dole zadajte url RSS",
+    CORS:'https://api.rss2json.com/v1/api.json?rss_url=',
+    items: [],
+    feed: "https://www.sme.sk/rss-title" 
+  },
+
+  created: function () {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData: function () {
+
+      this.$http.get(this.CORS + this.feed).then(response => {
+        this.items = response.body;
+        
+      });
+    },
+    changeHandler: function (event) {
+      this.feed = event.target.value;
+      console.log(this.feed);
+      this.fetchData();
+    },
+
+
+    changeHandler: function (event) {
+      this.feed = event.target.value;
+      this.fetchData();
+    },
+    clicked: function () {
+      console.log(this.items);
+    },
+    
+    sortDesc () {
+      this.items.items.sort(function(a,b){
+        return new Date(b.pubDate) - new Date(a.pubDate);
+      });
+    },
+    sortAsc () {
+        this.items.items.sort(function(a,b){
+          return new Date(b.pubDate) - new Date(a.pubDate);
+        }).reverse();
+    }
+  
+  } 
+});
